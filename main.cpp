e042a0b7811fc03ea9ecc86557d451f42be12456
stumpfeklinge <136319104+stumpfeklinge@users.noreply.github.com>
2023-11-06 19:57:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@
 #include <algorithm>
 using namespace std;
 
+int compare(const void* x1, const void* x2)
+{
+	return (*(int*)x1 - *(int*)x2);              // если результат вычитания равен 0, то числа равны, < 0: x1 < x2; > 0: x1 > x2
+}
+void quickSort(double* arr, int low, int high) {
+
 int main() {
 
 	int n;
@@ -40,18 +46,17 @@ int main() {
 	cout << "Время выполнения поиска моей фуенкцией =" <<fixed<<  (double)(endse2 - startse2) / CLOCKS_PER_SEC << endl;
 
 	clock_t startbu1 = clock();
-	//bubbleSort(arr,n);
+	//bubbleSort(arr1,n);
 	clock_t endbu1 = clock();
 	//cout << "Время выполнения сортировки моей фуенкцией bubble =" <<fixed<<  (double)(endbu1 - startbu1) / CLOCKS_PER_SEC << endl;
 
 	clock_t startqu2 = clock();
-	quickSort(arr, 0, n - 1);
+	quickSort(arr2, 0, n - 1);
 	clock_t endqu2 = clock();
 	cout << "Время выполнения сортировки моей фуенкцией quick_sort =" <<fixed<<  (double)(endqu2 - startqu2) / CLOCKS_PER_SEC << endl;
 
 	clock_t startqu1 = clock();
-	int size = sizeof(arr) / sizeof(arr[0]);
-	sort(arr, arr + size);
+	qsort(arr,n,sizeof(int),compare);
 	clock_t endqu1 = clock();
 	cout << "Время выполнения сортировки встроенной фуенкцией sort =" <<fixed<<  (double)(endqu1 - startqu1) / CLOCKS_PER_SEC << endl;
 	
