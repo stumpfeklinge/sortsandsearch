05557855f9e43158c4d2bd8d48602d62284a451f
stumpfeklinge <136319104+stumpfeklinge@users.noreply.github.com>
2023-11-06 20:13:11 +0300

    Update sort_quick.cpp

--- sort_quick.cpp
+++ sort_quick.cpp
@@ -5,12 +5,12 @@ void quickSort(double* arr, int low, int high) {
 	
 	double mid, count;
 	int f=low, L=high;
-	mid=arr[(f+L) / 2]; //вычисление опорного элемента
-	do
+	mid=arr[(f+L) / 2]; 
+	for(;f<L;)
 	{
 		while (arr[f]<mid) f++;
 		while (arr[L]>mid) L--;
-		if (f<=L) //перестановка элементов
+		if (f<=L) 
 		{
 			count=arr[f];
 			arr[f]=arr[L];
@@ -18,7 +18,7 @@ void quickSort(double* arr, int low, int high) {
 			f++;
 			L--;
 		}
-	}while(f<L);
+	}
 	if (low<L) quickSort(arr, low, L);
 	if (f<high) quickSort(arr, f, high);
 }
